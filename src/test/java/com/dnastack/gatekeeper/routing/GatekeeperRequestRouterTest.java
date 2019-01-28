59407a180683b8cc5fbfadd07769379f2bdfc67d
andrewglynn <andrew@dnastack.com>
2019-01-28 12:38:40 -0500

    Finish unit test.

--- src/test/java/com/dnastack/gatekeeper/routing/GatekeeperRequestRouterTest.java
+++ src/test/java/com/dnastack/gatekeeper/routing/GatekeeperRequestRouterTest.java
@@ -1,109 +1,96 @@
 package com.dnastack.gatekeeper.routing;
 
-import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.security.Key;
 import java.util.Date;
 
 import javax.crypto.spec.SecretKeySpec;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRequestWrapper;
-import javax.servlet.http.HttpServletResponse;
 import javax.xml.bind.DatatypeConverter;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Spy;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
 
-import com.dnastack.gatekeeper.auth.InboundEmailWhitelistConfiguration;
-import com.netflix.zuul.context.RequestContext;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.netflix.zuul.exception.ZuulException;
 
 import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
+@RunWith(org.mockito.junit.MockitoJUnitRunner.class)
 public class GatekeeperRequestRouterTest {
 
-
+    SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256; 
+    byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary("32126741023641032641023641036201264102367126410261026423762032763122146702664021");
+    Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+    
+	@InjectMocks
 	GatekeeperRequestRouter router = new GatekeeperRequestRouter();
 
-    @Value("${gatekeeper.beaconServer.url}")
-    private String beaconServerUrl;
-
-    @Autowired
-    private InboundEmailWhitelistConfiguration emailWhitelist;
-
-
-	@SuppressWarnings("unused")
+	@Spy
+	ObjectMapper objectMapper = new ObjectMapper();
+	
+	@Spy 
+	JwtParser jwtParser = Jwts.parser()
+            				.setSigningKey(signingKey);
+	
+	
 	@Before
 	public void setUp() throws Exception {
-
+		router.setBeaconServerUrl("http://example.com/beacon/");
 	}
 
-
+	
+	
+	@SuppressWarnings("unused")
 	@Test
 	public final void testRoute() throws ZuulException {
-		
-
-        RequestContext ctx = RequestContext.getCurrentContext();
-        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256; 
         long nowMillis = System.currentTimeMillis();
         Date now = new Date(nowMillis);
-     
-        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary("andrewglynnandrewglynnandrewglynnandrewglynnandrewglynnandrewglynnandrewglynnandrewglynnandrewglynn");
-        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
-
 
         @SuppressWarnings("deprecation")
-		JwtBuilder builder = Jwts.builder().setId("jwtoken")
+		JwtBuilder builder = Jwts.builder().setId("jwtToken")
                                     .setIssuedAt(now)
                                     .setSubject("test")
-                                    .setIssuer("dnastack")
+                                    .setIssuer("example")
                                     .signWith(signatureAlgorithm, signingKey);
      
         int ttlMillis = -20000;
 
-
         long expMillis = nowMillis + ttlMillis;
             Date exp = new Date(expMillis);
             builder.setExpiration(exp);
-
-     
-
         String jsonWebToken = builder.compact();
-        String value = "authScheme:jwt , authToken:";
-        value = value + jsonWebToken;
-        ctx.set("authToken", jsonWebToken);        
-        ctx.addZuulRequestHeader("authentication", value);
+        
+        String value = "bearer " + jsonWebToken;
+      
 
-        HttpServletRequest request = ctx.getRequest();
-        HttpServletResponse response = ctx.getResponse();
 
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        MockHttpServletResponse response = new MockHttpServletResponse(); 
+        request.setAuthType("Basic");
+        request.addHeader("authorization", value);
+        request.setRequestURI("http://example.com/beacon/");
+        request.setQueryString("param1=whatever");
+        request.setServletPath("/beacon/");
 
         URI targetUrl;
         try {
-        	request.setAttribute("requestURI", "/beacon/");
             targetUrl = router.route(request, response);
         } catch (URISyntaxException e) {
             throw new ZuulException(e, 500, "Routing logic failed");
         } catch (UnroutableRequestException e) {
             throw new ZuulException(e, e.getStatus(), e.getMessage());
         }
-
-        try {
-            ctx.setRouteHost(targetUrl.resolve("/").toURL());
-        } catch (MalformedURLException e) {
-            throw new ZuulException(e, 500, "Could not construct forward URI");
-        }
-        ctx.set(FilterConstants.REQUEST_URI_KEY, targetUrl.getPath());
         
-        Object decision;
-		decision = response.getHeader("X-Gatekeeper-Access-Decision");
-        assert(decision.equals("insufficient-credentials"));
+        assert(response.getHeader("X-Gatekeeper-Access-Decision").equals("expired-credentials"));
 
 
 
