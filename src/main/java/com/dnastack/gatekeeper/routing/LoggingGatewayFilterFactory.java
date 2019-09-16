4ea7527eac6e235ce4981ffeaee1779a2ae88f91
Jonathan Fuerth <jonathan@dnastack.com>
2019-09-16 11:23:30 -0400

    Add option to log inbound and outbound request headers

--- src/main/java/com/dnastack/gatekeeper/routing/LoggingGatewayFilterFactory.java
+++ src/main/java/com/dnastack/gatekeeper/routing/LoggingGatewayFilterFactory.java
@@ -17,19 +17,10 @@
 
 package com.dnastack.gatekeeper.routing;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.cloud.gateway.route.Route;
-import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
-import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Mono;
-
-import java.net.URI;
 
 /**
  * Logs requests and responses going through the gateway.
@@ -37,33 +28,15 @@ import java.net.URI;
 @Component
 public class LoggingGatewayFilterFactory extends AbstractGatewayFilterFactory {
 
-	private static final Logger log = LoggerFactory.getLogger(LoggingGatewayFilterFactory.class);
-	private static final URI UNKNOWN_ROUTE_PLACEHOLDER = URI.create("http://unknown-route/");
+	private final LoggingWebFilter loggingFilter;
+
+	@Autowired
+	public LoggingGatewayFilterFactory(LoggingWebFilter loggingFilter) {
+		this.loggingFilter = loggingFilter;
+	}
 
 	@Override
 	public GatewayFilter apply(Object config) {
-		return (exchange, chain) -> {
-			logRoutedRequest(exchange);
-			final long startTime = System.currentTimeMillis();
-			return chain.filter(exchange).then(Mono.fromRunnable(() -> logResponse(exchange, startTime)));
-		};
-	}
-
-	private void logRoutedRequest(ServerWebExchange exchange) {
-		ServerHttpRequest request = exchange.getRequest();
-		URI requestUri = request.getURI();
-
-		Route route = exchange.getAttribute(ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR);
-		URI routeUri = route == null ? UNKNOWN_ROUTE_PLACEHOLDER : route.getUri();
-
-		String requestPathAndQuery = requestUri.getPath() + "?" + requestUri.getQuery();
-		URI calculatedRoute = routeUri.resolve(requestPathAndQuery);
-		log.info(">>> {} {}", request.getMethodValue(), calculatedRoute);
-	}
-
-	private void logResponse(ServerWebExchange exchange, long startTime) {
-		ServerHttpResponse response = exchange.getResponse();
-		final long elapsedTime = System.currentTimeMillis() - startTime;
-		log.info("<<< HTTP {}: {} bytes in {}ms", response.getStatusCode(), response.getHeaders().getContentLength(), elapsedTime);
+		return (exchange, chain) -> loggingFilter.filter(exchange, chain::filter);
 	}
 }
