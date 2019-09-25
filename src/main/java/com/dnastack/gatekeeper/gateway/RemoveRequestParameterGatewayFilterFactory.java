c3c3c58a8040cde9b0710492be3da21679cc33f5
Max Barkley <max@dnastack.com>
2019-09-25 15:27:11 -0400

    Fix copy-pasta compilation error

--- src/main/java/com/dnastack/gatekeeper/gateway/RemoveRequestParameterGatewayFilterFactory.java
+++ src/main/java/com/dnastack/gatekeeper/gateway/RemoveRequestParameterGatewayFilterFactory.java
@@ -16,23 +16,21 @@
 
 package com.dnastack.gatekeeper.gateway;
 
-import java.net.URI;
-import java.util.Arrays;
-import java.util.List;
-
-import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.stereotype.Component;
-import reactor.core.publisher.Mono;
-
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Mono;
 
-import static org.springframework.cloud.gateway.support.GatewayToStringStyler.filterToStringCreator;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.springframework.util.CollectionUtils.unmodifiableMultiValueMap;
 
 /*
@@ -77,9 +75,7 @@ public class RemoveRequestParameterGatewayFilterFactory
 
 			@Override
 			public String toString() {
-				return filterToStringCreator(
-						RemoveRequestParameterGatewayFilterFactory.this)
-								.append("name", config.getName()).toString();
+				return RemoveRequestParameterGatewayFilterFactory.class.getSimpleName() + "[config=" + config + "]";
 			}
 		};
 	}
