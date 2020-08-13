93e5217a2275792673a730eb008b899315809e5c
Jim Vlasblom <jim@dnastack.com>
2020-08-13 14:39:30 -0400

    Updated forwarded headers filter to modify the Forwarded "proto" field with the value in X-Forwarded-Proto if the latter is present.  Otherwise, it will infer the proto from the request URI (Default Spring cloud gateway "ForwardedHeadersFilter" behavior).

--- src/main/java/com/dnastack/gatekeeper/gateway/ForwardedHeaderGatewayFilterFactory.java
+++ src/main/java/com/dnastack/gatekeeper/gateway/ForwardedHeaderGatewayFilterFactory.java
@@ -1,22 +1,31 @@
 package com.dnastack.gatekeeper.gateway;
 
+import com.dnastack.gatekeeper.util.XForwardUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.cloud.gateway.filter.factory.PreserveHostHeaderGatewayFilterFactory;
 import org.springframework.cloud.gateway.filter.headers.ForwardedHeadersFilter;
+import org.springframework.cloud.gateway.filter.headers.XForwardedHeadersFilter;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
+import org.thymeleaf.util.StringUtils;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.springframework.cloud.gateway.filter.headers.ForwardedHeadersFilter.FORWARDED_HEADER;
 import static org.springframework.cloud.gateway.support.GatewayToStringStyler.filterToStringCreator;
-import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.PRESERVE_HOST_HEADER_ATTRIBUTE;
 
 @Component
+@Slf4j
 public class ForwardedHeaderGatewayFilterFactory extends AbstractGatewayFilterFactory {
+
+    public static final String X_FORWARDED_PROTO_HEADER = "X-Forwarded-Proto";
 
     @Autowired
     private ForwardedHeadersFilter filter;
@@ -27,10 +36,31 @@ public class ForwardedHeaderGatewayFilterFactory extends AbstractGatewayFilterFa
     }
 
     private void updateHeaders(ServerWebExchange exchange, HttpHeaders headers){
+        // Set the forwarded header to be consistent with the URI, per the default behavior of
+        // Spring Cloud Gateway's "ForwardedHeadersFilter".
         HttpHeaders newHeaders = filter.filter(exchange.getRequest().getHeaders(), exchange);
         headers.put(FORWARDED_HEADER, newHeaders.get(FORWARDED_HEADER));
+
+        // Additionally, if X-forwarded-proto is set, overwrite the protocol in the forwarded header
+        // with the value from X-forwarded-proto
+        List<String> xFwdHeader = headers.get(X_FORWARDED_PROTO_HEADER);
+        if(xFwdHeader != null){
+            //if X-forwarded-proto header is present, set the forwarded header to use that protocol.
+            String proto = xFwdHeader.get(0);
+            log.debug("X-Forwarded-Proto is present, overwriting Forwarded: [proto=xxxx;...] with X-Forwarded-proto value [proto="+proto+";...]");
+            if(proto != null){
+                String currentFwdHeader = headers.get(FORWARDED_HEADER).get(0);
+                String[] pairs = currentFwdHeader.split(";");
+                List<String> pairList = Stream.of(pairs)
+                      .map(pair->pair.startsWith("proto") ? "proto="+proto : pair)
+                      .collect(Collectors.toList());
+
+                headers.put(FORWARDED_HEADER, List.of(StringUtils.join(pairList, ";")));
+            }
+        }
     }
 
+    @Override
     public GatewayFilter apply(Object config) {
         return new GatewayFilter() {
             @Override
