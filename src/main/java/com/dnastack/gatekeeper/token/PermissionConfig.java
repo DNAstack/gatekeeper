8ed7f84b9fc619270355e98e8eb41be22e089dfa
Jakub Stromský <kstromsky@gmail.com>
2021-08-02 15:29:18 +0200

    [#176930702] Update libs - spring-boot, spring-cloud and java-wallet-token-validator

--- src/main/java/com/dnastack/gatekeeper/token/PermissionConfig.java
+++ src/main/java/com/dnastack/gatekeeper/token/PermissionConfig.java
@@ -1,19 +1,29 @@
 package com.dnastack.gatekeeper.token;
 
+import brave.Tracing;
 import com.dnastack.auth.PermissionChecker;
 import com.dnastack.auth.PermissionCheckerFactory;
 import com.dnastack.auth.model.IssuerInfo;
 import com.dnastack.gatekeeper.config.InboundConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.net.URI;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
 @Configuration
 public class PermissionConfig {
     @Bean
-    public PermissionChecker permissionChecker(InboundConfiguration inboundConfig, TokenConfig tokenConfig, ConfiguredSigningKeyResolver issuerPubKeyResolver) {
+    public PermissionChecker permissionChecker(
+        InboundConfiguration inboundConfig,
+        TokenConfig tokenConfig,
+        ConfiguredSigningKeyResolver issuerPubKeyResolver,
+        @Value("${gatekeeper.app.url}") String policyEvaluationRequester,
+        @Value("${inbound.jwt[0].issuer}") String walletUrl,
+        Tracing tracing
+    ) {
         final Collection<IssuerInfo> issuerInfos = inboundConfig.getJwt()
             .stream()
             .map(config -> IssuerInfo.IssuerInfoBuilder.builder()
@@ -22,6 +32,7 @@ public class PermissionConfig {
                 .publicKeyResolver(issuerPubKeyResolver)
                 .build())
             .collect(Collectors.toList());
-        return PermissionCheckerFactory.create(issuerInfos);
+        String policyEvaluationUrl = URI.create(walletUrl).resolve("/policies/evaluations").toString();
+        return PermissionCheckerFactory.create(issuerInfos, policyEvaluationRequester, policyEvaluationUrl, tracing);
     }
 }
